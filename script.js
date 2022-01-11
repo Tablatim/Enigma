1b3260b541a718e6025f9fcfd1359e2cdf7ea550
Tablatim <60846476+Tablatim@users.noreply.github.com>
2022-01-12 00:10:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,80 @@
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-]
-
-
-var rotors = []
 function main() {
-    if(rotors.length != 0) {
-        coded = document.getElementById("sentencecode").value
-        if(coded != "") {
-            var mot_div = document.getElementById("mot");
-            document.getElementById('mot').innerHTML="";
-            var newContent = document.createElement("div")
-            var newContent2 = document.createElement("div")
-            var newContent3 = document.createElement("div")
-            for(e in coded){
-                newContent.className = "letter"
-                newContent.innerHTML += `<h1 class='letter-inner'>${coded[e]}</h1>`
-                mot_div.appendChild(newContent);
-                newContent2.className = "letter"
-                newContent2.innerHTML += `<h1 class='letter-inner'>${alphabet[(alphabet.indexOf(coded[e])+rotors[e%(rotors.length)])%26]}</h1>`
-                mot_div.appendChild(newContent2);
-                newContent3.className = "rotor"
-                newContent3.innerHTML += `
-                <h1 class="rotor-nbr">
-                    <input type="number" class="rotor-nbr-inner" min=0 max=26 value=${rotors[e%(rotors.length)]} id="${e}" onclick="rotors[this.id%(rotors.length)] = parseInt(document.getElementById(this.id).value);"></input>
-                    </br><button class="rotor-btn" id="${e}+" onclick="if(parseInt(document.getElementById(this.id.slice(0, -1)).value) > 0) rotors[this.id.slice(0, -1)%(rotors.length)] = parseInt(document.getElementById(this.id.slice(0, -1)).value)-1;else{ rotors[this.id.slice(0, -1)%(rotors.length)] = 25-parseInt(document.getElementById(this.id.slice(0, -1)).value)}">-</button>
-                    <button class="rotor-btn" id="${e}-" onclick="if(parseInt(document.getElementById(this.id.slice(0, -1)).value) == 25)rotors[this.id.slice(0, -1)%(rotors.length)] = 0;else{ rotors[this.id.slice(0, -1)%(rotors.length)] = parseInt(document.getElementById(this.id.slice(0, -1)).value)+1 }">+</button>
-                </h1>`
-                mot_div.appendChild(newContent3);
-            }
-        }
-    } else {
-        if(!isNaN(document.getElementById("nbr-rotors").value) && document.getElementById("nbr-rotors").value!="") {
-            for(i=0; i<parseInt(document.getElementById("nbr-rotors").value); i++){
-                rotors.push(0)
-            }
-        }
-    }
+    SpacePressed()
+    SupprPressed()
+    blink()
 }
 
-main()
-setInterval(() => {
-    main()
-}, 500);
+var sentence = []
+var sentencecode = []
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+function blink() {
+    document.body.addEventListener('keydown', (e) => {
+        if(!alphabet.includes(e.key)) return
+        var rotorsvalues = random()
+        var k = alphabet.indexOf(e.key)
+        if(k >= 20) {
+            k = k - (rotorsvalues[0]%10)
+        }
+        var newIndex = (k + rotorsvalues[0] + rotorsvalues[1] + rotorsvalues[2]) % 26
+        var key = document.getElementById(alphabet[newIndex]) || false
+        if(key) {
+            key.style.backgroundColor = "#E4FD67"
+            key.style.boxShadow = "rgba(0, 0, 0, 0.2) 0 -1px 7px 1px, inset #C3FE76 0 -1px 9px, #E4FD67 0 2px 12px"
+            sentence.push(e.key)
+            sentencecode.push(alphabet[newIndex])
+            document.getElementById("sentence").innerHTML = sentence.join('')
+            document.getElementById("sentencecode").innerHTML = sentencecode.join('')
+            setTimeout(function() {
+                document.getElementById(alphabet[newIndex]).style.backgroundColor = "white"
+                document.getElementById(alphabet[newIndex]).style.boxShadow = ""
+            }, 250)
+        }
+    });
+}
+
+function SpacePressed() {
+    document.body.addEventListener('keydown', (e) => {
+        if(e.key == ' ') {
+            sentence.push("_")
+            sentencecode.push("_")
+            document.getElementById("sentence").innerHTML = sentence.join('')
+            document.getElementById("sentencecode").innerHTML = sentencecode.join('')
+            console.log(sentence)
+        }
+    });
+}
+
+function SupprPressed() {
+    document.body.addEventListener('keydown', (e) => {
+        if(e.keyCode == 8) {
+            sentence.pop()
+            sentencecode.pop()
+            document.getElementById("sentence").innerHTML = sentence.join('')
+            document.getElementById("sentencecode").innerHTML = sentencecode.join('')
+        }
+    });
+}
+
+function copy(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+    alert(`Vous avez copier : '${$(element).text()}'`)
+}
+
+function random() {
+    var rotor1 = document.getElementById("first")
+    var rotor2 = document.getElementById("second")
+    var rotor3 = document.getElementById("third")
+    var v1 = Math.floor(Math.random() * 26)
+    var v2 = Math.floor(Math.random() * 26)
+    var v3 = Math.floor(Math.random() * 26)
+    rotor1.innerHTML = v1
+    rotor2.innerHTML = v2
+    rotor3.innerHTML = v3
+    return [v1, v2, v3]
+}
+
+main() 
